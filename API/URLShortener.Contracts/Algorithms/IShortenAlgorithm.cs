a53b426c11e4dc67d61a795a4eb1f0b06e0cab75
Amin Mohammadi <amin.game12@gmail.com>
2022-02-02 10:43:30 +0330

    Added shorten algorithm interface to enable use of different algorithms.

---
+++ API/URLShortener.Contracts/Algorithms/IShortenAlgorithm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Contracts.Algorithms
+{
+    public interface IShortenAlgorithm
+    {
+        public string Apply(string input);
+    }
+}
