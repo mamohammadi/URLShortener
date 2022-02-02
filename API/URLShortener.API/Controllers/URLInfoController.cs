d7e77fede0af4f844971c3c673205992e03e22cb
Amin Mohammadi <amin.game12@gmail.com>
2022-02-02 11:52:17 +0330

    Added url info controller (hitcount + info).

---
+++ API/URLShortener.API/Controllers/URLInfoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using URLShortener.Contracts.DTO;
+using URLShortener.Contracts.Services;
+
+namespace URLShortener.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class URLInfoController : Controller
+    {
+        private IURLInfoService _urlInfoService;
+
+        public URLInfoController(IURLInfoService urlInfoService)
+        {
+            _urlInfoService = urlInfoService;
+        }
+
+        [HttpGet]
+        [Route("hitcount")]
+        public async Task<ActionResult<long>> HitCount(string shortUrl)
+        {
+            return await _urlInfoService.GetURLHitCountAsync(shortUrl);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<ActionResult<ICollection<URLInfoDTO>>> All()
+        {
+            return (await _urlInfoService.GetAllURLsAsync()).ToList();
+        }
+    }
+}
