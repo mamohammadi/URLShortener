d1ea35f7d425e11d61e94027e95d3a97e230df31
Amin Mohammadi <amin.game12@gmail.com>
2022-02-02 09:16:40 +0330

    Added URL entity.

---
+++ API/URLShortener.Contracts/Data/Entities/URL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Contracts.Data.Entities
+{
+    public class URL
+    {
+        public Guid Id { get; set; }
+        public string ShortURLVersion { get; set; }
+        public string LongURLVersion { get; set; }
+        public long HitCount { get; set; }
+    }
+}
