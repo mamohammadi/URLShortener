76b6a25a4bd44a448117f402775eb0f901964697
Amin Mohammadi <amin.game12@gmail.com>
2022-02-02 10:50:28 +0330

    Added basic shorten algorithm using hash method.

---
+++ API/URLShortener.Core/Algorithms/HashingAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using URLShortener.Contracts.Algorithms;
+
+namespace URLShortener.Core.Algorithms
+{
+    public class HashingAlgorithm : IShortenAlgorithm
+    {
+        public string Apply(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return null;
+            using (var method = SHA256.Create())
+                return Convert.ToBase64String(method.ComputeHash(Encoding.UTF8.GetBytes(input)));
+        }
+    }
+}
